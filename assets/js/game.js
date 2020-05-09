eeda47bc0603819b7e9cce92ff97a6999bcf6e36
Deruy <thomasderuy@hotmail.fr>
2020-05-09 15:10:05 +0200

    Table_V2+Affichage adapté

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,47 +3,51 @@ let Affichage = (function(){
     return{
         addSlick:()=>{
             $(document).ready(function(){
-                console.log('tastqe')
                 $('.cadre').slick({
                     infinite: true,
                     speed: 300,
-                    slidesToShow: 5,
-                    slidesToScroll: 5,
+                    slidesToShow: 4,
+                    slidesToScroll: 3,
                 });
             });
         },
-        addNewSlick:(name, titre, description, cout)=>{
+        addNewSlick:(name, titre, description, cout,categorie)=>{
             let NewSlick = document.createElement("div")
+            NewSlick.setAttribute("class", categorie)
             let titre_ = document.createElement("h2")
+            titre_.setAttribute("class","ecriture1")
             titre_.appendChild(document.createTextNode(titre))
-            let description_= document.createElement("h3")
+            let description_= document.createElement("h4")
+            description_.setAttribute("class","ecriture2")
             description_.appendChild(document.createTextNode(description))
-            let cout_= document.createElement("h3")
+            let cout_= document.createElement("h4")
+            cout_.setAttribute("class","ecriture2 ")
             cout_.appendChild(document.createTextNode("cout :"+cout))
             let coche = document.createElement("input")
-            coche.setAttribute("type","checkbox")
-            coche.setAttribute("value","1")
-            coche.setAttribute("id",name+"valider")
-            NewSlick.setAttribute("id",name)
+            coche.setAttribute("type","radio")
+            coche.setAttribute("value",name)
+            coche.setAttribute("name",categorie)
+            coche.setAttribute("id",name)
             NewSlick.appendChild(titre_)
             NewSlick.appendChild(description_)
             NewSlick.appendChild(cout_)
             NewSlick.appendChild(coche)
             document.getElementById("cadre").appendChild(NewSlick)
         },
-        isValider:(name)=>{
-            if(document.getElementById(name+"valider").value==1){
-
-            }
+        isValider:(Categorie)=>{
+            let elementChoisit = document.getElementsByName(Categorie).value
         }
     }
 })();
 
 
 Affichage.addSlick();
-Affichage.addNewSlick("dossier1","dossier1","dossier1",100)
-Affichage.addNewSlick("dossier2","dossier2","dossier2",1000)
-Affichage.addNewSlick("dossier3","dossier3","dossier3",10000)
-Affichage.addNewSlick("dossier4","dossier4","dossier4",95148)
-Affichage.addNewSlick("dossier5","dossier5","dossier5",81448)
-Affichage.addNewSlick("dossier6","dossier6","dossier6",85845)
+Affichage.addNewSlick("dossier1","dossier1","dossier1","100","amelioration_social")
+Affichage.addNewSlick("dossier2","dossier2","dossier2","1000","amelioration_social")
+Affichage.addNewSlick("dossier3","dossier3","dossier3","10000","ponctuel_production")
+Affichage.addNewSlick("dossier4","dossier4","dossier4",95148,"ponctuel_publicite")
+Affichage.addNewSlick("dossier5","dossier5","dossier5",81448,"ponctuel_publicite")
+Affichage.addNewSlick("dossier6","dossier6","dossier6",85845,"ponctuel_publicite")
+Affichage.addNewSlick("dossier7","dossier7","dossier7","10000","ponctuel_ecologie")
+Affichage.addNewSlick("dossier8","dossier8","dossier8","100","ponctuel_ecologie")
+Affichage.addNewSlick("dossier8","dossier8","dossier8","1000","ponctuel_ecologie")
